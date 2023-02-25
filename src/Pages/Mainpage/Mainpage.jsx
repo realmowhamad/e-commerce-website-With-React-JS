5a46c955fb35c474d378386d2be80816423393f9
Mohammad Mirhoseini <Realmowhamad@gmail.com>
2023-02-25 17:16:10 +0330

    comlete Product UI

--- src/Pages/Mainpage/Mainpage.jsx
+++ src/Pages/Mainpage/Mainpage.jsx
@@ -3,6 +3,7 @@ import Product from "../../Components/Product/Product";
 import { useDispatch, useSelector } from "react-redux";
 import { getAsyncProducts } from "../../Features/ProductSlice/ProductSlice";
 import Loader from "../../Components/Loader/Loader";
+import { BsChevronRight } from 'react-icons/bs'
 
 export default function Mainpage() {
     const ProductState = useSelector(state => state.Products)
@@ -12,7 +13,7 @@ export default function Mainpage() {
 
     useEffect(() => {
         Dispatch(getAsyncProducts())
-        console.log(loaded, error, products);
+
     }, [])
 
 
@@ -26,13 +27,13 @@ export default function Mainpage() {
     if (error) return <p>{error}</p>
     if (products.length > 0) return (
         <div className="home__container">
-            <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/9abd25855843b1e5dbe7cd8ea525c97a5b81ed3e_1677040633.gif?x-oss-process=image" alt="" />
-            <div className="category__container flex flex-col">
-                <div className="home__row flex flex-wrap justify-between px-5">
-                    <p>
+            {/* <img src="https://dkstatics-public.digikala.com/digikala-adservice-banners/9abd25855843b1e5dbe7cd8ea525c97a5b81ed3e_1677040633.gif?x-oss-process=image" alt="" /> */}
+            <div className="category__container flex flex-col my-2">
+                <div className="home__row flex my-2 flex-wrap justify-between px-2 font-sfp_Regular text-cyan-400">
+                    <p className="bg-neutral-700 p-2 rounded-lg text-white">
                         Mens collection
                     </p>
-                    <p>watch more ...</p>
+                    <p className="flex items-center"><span className="mx-2">Watch more</span> <BsChevronRight /></p>
                 </div>
                 {/* Products show here */}
                 <div className="flex items-center justify-start overflow-auto">
@@ -43,11 +44,11 @@ export default function Mainpage() {
 
             </div>
             <div className="category__container flex flex-col">
-                <div className="home__row flex flex-wrap justify-between px-5">
-                    <p>
-                        Mens collection
+                <div className="home__row flex my-2 flex-wrap justify-between px-2 font-sfp_Regular text-cyan-400">
+                    <p className="bg-neutral-700 p-2 rounded-lg text-white">
+                        Women collection
                     </p>
-                    <p>watch more ...</p>
+                    <p className="flex items-center"><span className="mx-2">Watch more</span> <BsChevronRight /></p>
                 </div>
                 {/* Products show here */}
                 <div className="flex items-center justify-start overflow-auto">
