cd1c692e347462cbc1ab4ced071b1e0bd36246ed
Mohammad Mirhoseini <Realmowhamad@gmail.com>
2023-03-05 14:28:35 +0330

    landing page mobile responsive

--- src/Pages/Mainpage/Mainpage.jsx
+++ src/Pages/Mainpage/Mainpage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Product from "../../Components/Product/Product";
 import { useDispatch, useSelector } from "react-redux";
 import { getAsyncProducts } from "../../Features/ProductSlice/ProductSlice";
@@ -6,12 +6,14 @@ import Loader from "../../Components/Loader/Loader";
 import Crausel from "../../Components/Crausel/Crausel";
 import Banner from '../../Assets/Images/Banner-BestSeller.jpg'
 import { Link } from "react-router-dom";
-
+import modelImage from '../../Assets/Images/girl-hat.png'
+import style from './Mainpage.module.css'
 
 export default function Mainpage() {
     const ProductState = useSelector(state => state.Products)
     const { loaded, error, products } = ProductState
     const Dispatch = useDispatch()
+    const ref = useRef()
 
 
     useEffect(() => {
@@ -19,14 +21,29 @@ export default function Mainpage() {
 
     }, [])
 
+    const scrollHandler = () => {
+        ref.current?.scrollIntoView({ behavior: 'smooth' })
+    }
+
 
     if (loaded) return <Loader />
     if (error) return <p>{error}</p>
     if (products.length > 0) return (
-        <div className="home__container">
+        <div className="home__container w-screen h-screen overflow-y-scroll">
+            <section className={`${style.mainSection} w-screen h-screen overflow-hidden flex flex-col p-2 `}>
+                <div className=" h-2/6 rounded-t-[10px] items-center flex flex-col text-white  font-semibold text-2xl mt-2 relative">
+                    <p className="">Whatever you see,</p>
+                    <p className=" ">Can <span className='text-[40px]'>INSPIRE</span> you</p>
+                    <p className="font-semibold text-sm text-justify px-9">give your eyes the stylish designed protection they deserve at affordable prices with amazing quality.</p>
+                    <button className="w-3/6 text-xl px-5 py-2 mt-8 rounded-[10px] text-[#faa434] " onClick={scrollHandler}>shop now</button>
+                </div>
+                <img src={modelImage} alt="" />
+
+
+            </section>
             <Crausel />
             {/*  show Products */}
-            <div className="flex flex-wrap justify-center p-2 mt-2 laptop:items-center laptop:justify-around">
+            <div ref={ref} className="flex flex-wrap justify-center p-2 mt-2 laptop:items-center laptop:justify-around">
 
                 {products.slice(0, 4).map(product => (
                     <Product key={product.id} {...product} />
